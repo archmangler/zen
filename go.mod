246d436274d8bcda0e928620c307fe630f4084db
Traiano Welcome <traiano@gmail.com>
2024-06-09 21:20:35 +0800

    module basics

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/archmangler/zen
+
+go 1.21.6
---
+++ zen.go
@@ -0,0 +1,6 @@
+package zen
+
+// returns a koan
+func zen() string {
+	return "Seijo's soul separated from her being. Which was the real Seijo?"
+}
